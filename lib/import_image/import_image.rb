b3d094ea33aa8ff9537c5eed69a5ab5beaa4a18a
Jonny Dalgleish <fighella@gmail.com>
2016-05-29 20:50:32 +1000

    Updated!

--- lib/import_image/import_image.rb
+++ lib/import_image/import_image.rb
@@ -11,7 +11,7 @@ module ImageImports
 
     def self.process_all_images(path, token)
       if path and token
-        Product.all_products_array.each do |page|
+        ImageImports::Product.all_products_array.each do |page|
           page.each do |product|
             ImportImage.new(product,path,token).update_images
           end
