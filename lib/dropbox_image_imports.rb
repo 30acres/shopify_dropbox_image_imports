a0430d905834c8dac1723086379668450bac215a
Jonny Dalgleish <fighella@gmail.com>
2016-08-06 18:37:22 +1000

    11

--- lib/dropbox_image_imports.rb
+++ lib/dropbox_image_imports.rb
@@ -4,15 +4,10 @@ class DropboxImageImports
   require 'dropbox_image_imports/notification' 
   require 'dropbox_image_imports/crunch' 
 
-  
   def initialize(path, token)
     @path = path
     @token = token
   end
-  # require "dropbox_image_imports/product"
-  # require "dropbox_image_imports/process"
-  # require "dropbox_image_imports/import"
-  # require "dropbox_image_imports/notification"
   
   def update_all_products
     Crunch.process_all_images(@path,@token)
