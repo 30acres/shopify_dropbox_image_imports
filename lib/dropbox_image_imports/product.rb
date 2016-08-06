a484b23e6c9adf2bb65f7c14e2a87b00e1b1cdfd
Jonny Dalgleish <fighella@gmail.com>
2016-08-07 09:16:38 +1000

    11

--- lib/dropbox_image_imports/product.rb
+++ lib/dropbox_image_imports/product.rb
@@ -29,22 +29,24 @@ class DropboxImageImports::Product
   def self.reorder_images
     #reload the product and check on the images
     DropboxImageImports::Notification.notify("Image Order Check")
-    self.all_products_array.each do |product|
-      changed_images = []
-      product.images.each do |img|
-        intended_position = img.src.split('-').last.split('.').first.gsub(/[^0-9,.]/,'').to_i + 1
-        if intended_position != img.position
-          img.position = intented_position
-          changed_images << img.src
-          if ShopifyAPI.credit_left <= 39
-            puts 'Snoozed'
-            sleep(20)
+    self.all_products_array.each do |page|
+      page.each do |product|
+        changed_images = []
+        product.images.each do |img|
+          intended_position = img.src.split('-').last.split('.').first.gsub(/[^0-9,.]/,'').to_i + 1
+          if intended_position != img.position
+            img.position = intented_position
+            changed_images << img.src
+            if ShopifyAPI.credit_left <= 39
+              puts 'Snoozed'
+              sleep(20)
+            end
           end
         end
-      end
-      if changed_images.any?
-        DropboxImageImports::Notification.notify("Reordered: #{product.title}")
-        product.save!
+        if changed_images.any?
+          DropboxImageImports::Notification.notify("Reordered: #{product.title}")
+          product.save!
+        end
       end
     end
   end
