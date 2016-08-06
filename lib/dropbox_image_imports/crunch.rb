f20f232bf61d68c8daf63d1471c05a19a67dd7e5
Jonny Dalgleish <fighella@gmail.com>
2016-08-06 19:07:10 +1000

    11

--- lib/dropbox_image_imports/crunch.rb
+++ lib/dropbox_image_imports/crunch.rb
@@ -4,17 +4,17 @@ require "fastimage"
 
 class DropboxImageImports::Crunch  < DropboxImageImports::Source
 
-  def process_all_images
+  def self.process_all_images
     Notification.notify('Process Started')
     process_all
     Notification.notify('Process Finished')
   end
 
-  def process_missing_images
+  def self.process_missing_images
     process_all_images ## Same for now
   end
 
-  def process_all
+  def self.process_all
     if @path and @token
       Product.all_products_array.each do |page|
         page.each do |product|
