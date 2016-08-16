6b9076db8cda877b1ba3246c4fac90013e53fe9d
Jonny Dalgleish <fighella@gmail.com>
2016-08-16 20:40:16 +1000

    Stop Import

--- lib/dropbox_image_imports/import.rb
+++ lib/dropbox_image_imports/import.rb
@@ -43,6 +43,7 @@ class DropboxImageImports::Import < DropboxImageImports::Source
       paths = @source.path.split(',')
       images = []
       paths.each do |path|
+        byebug
         images = images + connect_to_source.metadata(path)['contents'].select { |image| image['path'] if image['path'].downcase.include?(@product.variants.first.sku.downcase + '-')   }
       end
       images
