ec826393655f64b9d23eeb6f30fdf80f6dc37168
Jonny Dalgleish <fighella@gmail.com>
2016-08-16 22:15:56 +1000

    frit

--- lib/dropbox_image_imports/import.rb
+++ lib/dropbox_image_imports/import.rb
@@ -118,7 +118,7 @@ class DropboxImageImports::Import < DropboxImageImports::Source
     if @product.images and dropbox_images
       dim = dropbox_images.map { |di| di["modified"].to_time.to_i }.sort { |x, y| x.to_i <=> y.to_i }.last
       pim = @product.images.map { |pi| ShopifyAPI::Metafield.find(:first,:params=>{:resource => "product_images", :resource_id => pi.id } ).value.to_time.to_i if ShopifyAPI::Metafield.find(:first,:params=>{:resource => "product_images", :resource_id => pi.id} )  }.sort { |x, y| x.to_i <=> y.to_i }.last
-      if dim > pim
+      if dim and pim and dim > pim
         changed = true
       end
     end
